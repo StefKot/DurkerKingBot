7825d8ff07be3b82af1b19b4bfdfdb5311d8ad7b
StefKot <StefanKotov22@gmail.com>
2024-08-12 21:31:04 +0300

    Test

---
+++ bot/main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher
+
+import asyncio
+
+import config
+
+bot = Bot(token=config.token)
+dp = Dispatcher(bot)
+
+async def main():
+    from handlers import dp
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.session.close()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print('Bot stopped!')
